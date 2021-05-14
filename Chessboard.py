65aae225921e4105679a5f526c3bb9e545a33dec
WiktorKS <wiktorks1994@gmail.com>
2021-05-14 19:48:50 +0200

    Piece movement and game logic implementation

--- Chessboard.py
+++ Chessboard.py
@@ -1,13 +1,122 @@
 import numpy as np
-from Pawn import ChessEntity, Pawn
+from Pawn import Piece, Pawn, Bishop, Knight, Rook, King, Queen
+
+
+class ChessError(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
 
 class ChessBoard:
+    letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+
     def __init__(self):
-        self.chessboard = np.array([None for _ in range(64)], dtype=ChessEntity).reshape(8, 8)
-        self.chessboard[7, 1] = Pawn(7, 1)
+        self.chessboard = np.array(
+            [None for _ in range(64)], dtype=Piece).reshape(8, 8)
+        for i in range(8):
+            self.chessboard[i, 1] = Pawn(i, 1, 'W')
+
+        for i in range(8):
+            self.chessboard[i, 6] = Pawn(i, 6, 'B')
+# popraw rozmieszczenie pionów ( w pętli)
+        self.chessboard[2, 0] = Bishop(2, 0, 'W')
+        self.chessboard[5, 0] = Bishop(5, 0, 'W')
+        self.chessboard[2, 7] = Bishop(2, 7, 'B')
+        self.chessboard[5, 7] = Bishop(5, 7, 'B')
+
+        self.chessboard[1, 0] = Knight(1, 0, 'W')
+        self.chessboard[6, 0] = Knight(6, 0, 'W')
+        self.chessboard[1, 7] = Knight(1, 7, 'B')
+        self.chessboard[6, 7] = Knight(6, 7, 'B')
+
+        self.chessboard[0, 0] = Rook(0, 0, 'W')
+        self.chessboard[7, 0] = Rook(7, 0, 'W')
+        self.chessboard[0, 7] = Rook(0, 7, 'B')
+        self.chessboard[7, 7] = Rook(7, 7, 'B')
+
+        self.chessboard[4, 0] = King(4, 0, 'W')
+        self.chessboard[4, 7] = King(4, 7, 'B')
+
+        self.chessboard[3, 0] = Queen(3, 0, 'W')
+        self.chessboard[3, 7] = Queen(3, 7, 'B')
+
+    def print_board(self):
+        for x in range(8):
+            print(f'{self.letters[x]}[ ', end='')
+            for y in range(8):
+                if self.chessboard[x, y]:
+                    print(f'{str(self.chessboard[x, y])} ', end='')
+                else:
+                    print('O ', end='')
+            print(']')
+        print(f'X[ {"".join([f"{i} " for i in range(1, 9)])}]')
+
+    # Ogranicz niepotrzebne pętle w wyborze pionów
+    def get_player_move(self, input_message, turn, selected_pawn=None):
+        while True:
+            player_input = input(input_message)
+            if player_input == 'board':
+                self.print_board()
+            elif selected_pawn and player_input == 'cancel':
+                return player_input
+            else:
+                try:
+                    # Spróbuj ze słownikiem pól szachownicy: {"A1": (0, 0), ...}
+                    coordinates = player_input.split(' ')[:2]
+                    if coordinates[0].upper() not in self.letters or int(coordinates[1]) in list(range(1, 9)):
+                        raise ChessError(
+                            'First value must be letter from A to H and second value must be integer from 1 to 8')
+
+                    coordinates = self.letters.index(
+                        coordinates[0].upper()), int(coordinates[1]) - 1
+                    # {
+                    # "A1" : (0,0) 
+                    # }
+                    if selected_pawn:
+                        moves, attacks = selected_pawn.get_available_moves(
+                            self.chessboard)
+                        if coordinates not in moves and coordinates not in attacks:
+                            raise ChessError('Illegal move')
+                    else:
+                        temp_piece = self.chessboard[coordinates]
+                        if not temp_piece or temp_piece.color != turn:
+                            raise ChessError('Choose a figure of your color.')
+
+                    return coordinates
+                except ChessError as chessErr:
+                    print(chessErr)
+
+    def game(self):
+        turn = 'W'
+        print('--------------Chess Terminal game------------')
+        print('For board display type "board" in console')
+        while True:
+            print('White turn') if turn == 'W' else print('Black turn')
+            self.print_board()
+
+            while True:
+                piece_coordinates = self.get_player_move(
+                    'Select the piece you want to move. Type "board" in console to display chessboard\n',
+                    turn
+                )
+                piece = self.chessboard[piece_coordinates]
+                move_coordinates = self.get_player_move(
+                    'Select place you want your piece to move. Type "board" in console to display chessboard.\nType "cancel" to deselect the piece.\n',
+                    turn,
+                    selected_pawn=piece
+                )
+                if move_coordinates != 'cancel':
+                    break
+
+            old_position = piece.x, piece.y
+
+            self.chessboard[move_coordinates] = piece
+            self.chessboard[old_position] = None
+            piece.move(move_coordinates)
+            
+            turn = 'B' if turn == 'W' else turn = 'W' 
 
 
 if __name__ == '__main__':
-    a = np.array([None for _ in range(64)], dtype=Pawn).reshape(8, 8)
-    a[7, 1] = Pawn(7, 1, 'Black')
-    print(a[7, 1].color)
+    game = ChessBoard()
+    game.game()
--- Pawn.py
+++ Pawn.py
@@ -1,47 +1,165 @@
 import numpy as np
 
 # string przy kolorze
-# BS BQ WS WQ  black b Black B 
-class ChessEntity:
+# BS BQ WS WQ  black b Black B
+
+
+class Piece:
     def __init__(self, x, y, color):
         self.x = x
         self.y = y
         self.color = color
-        self.start = True
-    
+        self.start_position = True
+
     def move(self, move):
         for x in move:
-            if x not in range(0, 7):
+            if x not in range(0, 8):
                 raise ValueError('x should be between 0 and 7')
-        
-        self.x, self.y = move
 
-# dekorator property (metody jako atrybuty)
-# popraw metodę, uwzględnij ruchy czarnego piona (property nie przejdzie, )
-# Google excel do notowania ile czasu upłynęło
-class Pawn(ChessEntity):
-    def available_moves(self, chessboard):
+        self.x, self.y = move
+        self.start_position = False
+
+    def check_move(self, move):
+        return all(x < y and x >= 0 for x, y in zip(move, (8, 8)))
+
+    def check_diagonal(self, direction, chessboard):
         available_moves = []
         available_attacks = []
-        if chessboard[self.x, self.y+1] is None:
-            available_moves.append((self.x, self.y+1))
-            if self.start and chessboard[self.x, self.y+2] is None: 
-                available_moves.append((self.x, self.y+2))
+        # [1, 1] + [0, 1] = [1, 2]
+        position = np.array([self.x, self.y]) + direction
 
-        attack_fields = (chessboard[self.x + 1, self.y + 1], chessboard[self.x - 1, self.y + 1])
-        for attack_field in attack_fields:
-            if attack_field is not None and attack_field.color != self.color:
-                available_attacks.append((attack_field.x, attack_field.y))
-        
+        while np.all(position >= 0) and np.all(position < 8):
+            if self.check_move(tuple(position)):
+                if chessboard[tuple(position)] == None:
+                    available_moves.append(tuple(position))
+                    position += direction
+
+                else:
+                    if isinstance(chessboard[tuple(position)], Piece) and chessboard[tuple(position)].color != self.color:
+                        available_attacks.append(tuple(position))
+                    break
+
         return available_moves, available_attacks
 
-# logikę dać do chessboard, nie powinien zmieniać pól innej klasy
-    def move(self, move, chessboard):
-        x, y = move
-        chessboard[x, y] = self
-        chessboard[self.x, self.y] = None
-        super().move(move)
+# dekorator property (metody jako atrybuty)
+# popraw metodę, uwzględnij ruchy czarnego piona (property nie przejdzie, trzeba dodatkowego argumentu)
+# Google excel do notowania ile czasu upłynęło
 
+
+class Pawn(Piece):
+    def get_available_moves(self, chessboard):
+        available_moves = []
+        available_attacks = []
+
+        direction = 1 if self.color == 'W' else -1
+
+        if self.check_move((self.x, self.y + direction)) and not chessboard[(self.x, self.y + direction)]:
+            available_moves.append((self.x, self.y + direction))
+
+        if self.start_position and self.check_move((self.x, self.y + direction * 2)) and not chessboard[(self.x, self.y + direction * 2)]:
+            available_moves.append((self.x, self.y + direction * 2))
+
+        for attack in [(self.x + 1, self.y + direction), (self.x - 1, self.y + direction)]:
+            if self.check_move(attack) and chessboard[attack]:
+                available_attacks.append(attack)
+
+        return available_moves, available_attacks
 
     def __str__(self):
-        return 'P'
+        return 'P' if self.color == 'W' else 'p'
+
+
+class Bishop(Piece):
+    def get_available_moves(self, chessboard):
+        available_moves = []
+        available_attacks = []
+
+        for direction in [[1, 1], [1, -1], [-1, -1], [-1, 1]]:
+            moves, attacks = self.check_diagonal(direction, chessboard)
+            if moves:
+                available_moves += moves
+            if attacks:
+                available_attacks += attacks
+
+        return available_moves, available_attacks
+
+    def __str__(self):
+        return 'B' if self.color == 'W' else 'b'
+
+
+class Knight(Piece):
+    def get_available_moves(self, chessboard):
+        available_moves = []
+        available_attacks = []
+        current_position = np.array([self.x, self.y])
+
+        for direction in [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]]:
+            next_position = current_position + direction
+            if self.check_move(next_position):
+                if chessboard[next_position] == None:
+                    available_moves.append(tuple(next_position))
+                elif isinstance(chessboard[next_position], Piece) and chessboard[next_position].color != self.color:
+                    available_attacks.append(tuple(next_position))
+
+        return available_moves, available_attacks
+
+    def __str__(self):
+        return 'H' if self.color == 'W' else 'h'
+
+
+class Rook(Piece):
+    def get_available_moves(self, chessboard):
+        available_moves = []
+        available_attacks = []
+
+        for direction in [[0, 1], [0, -1], [-1, 0], [1, 0]]:
+            moves, attacks = self.check_diagonal(direction, chessboard)
+            if moves:
+                available_moves += moves
+            if attacks:
+                available_attacks += attacks
+
+        return available_moves, available_attacks
+
+    def __str__(self):
+        return 'R' if self.color == 'W' else 'r'
+
+
+class King(Piece):
+    def get_available_moves(self, chessboard):
+        available_moves = []
+        available_attacks = []
+        current_position = np.array([self.x, self.y])
+
+        for direction in [[0, 1], [0, -1], [-1, 0], [1, 0], [1, 1], [1, -1], [-1, -1], [-1, 1]]:
+            next_position = current_position + direction
+
+            if self.check_move(tuple(next_position)):
+                if chessboard[next_position] == None:
+                    available_moves.append(tuple(next_position))
+
+                elif isinstance(chessboard[next_position], Piece) and chessboard[next_position].color != self.color:
+                    available_attacks.append(tuple(next_position))
+
+        return available_moves, available_attacks
+
+    def __str__(self):
+        return 'K' if self.color == 'W' else 'k'
+
+
+class Queen(Piece):
+    def get_available_moves(self, chessboard):
+        available_moves = []
+        available_attacks = []
+
+        for direction in [[0, 1], [0, -1], [-1, 0], [1, 0], [1, 1], [1, -1], [-1, -1], [-1, 1]]:
+            moves, attacks = self.check_diagonal(direction, chessboard)
+            if moves:
+                available_moves += moves
+            if attacks:
+                available_attacks += attacks
+
+        return available_moves, available_attacks
+
+    def __str__(self):
+        return 'Q' if self.color == 'W' else 'q'
