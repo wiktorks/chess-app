56cdae6a2828011e608068abce9631acfe0b4535
WiktorKS <wiktorks1994@gmail.com>
2021-06-25 16:24:28 +0200

    Urls refactor

--- ChessApp/urls.py
+++ ChessApp/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from users.rest_views import UserViewSet
 
-router = DefaultRouter()
-router.register('users', UserViewSet, basename='users')
+# router = DefaultRouter()
+# router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('admin/', admin.site.urls),  
-    path('auth/', include(router.urls))
+    # path('auth/', include(router.urls))
+    path('sample/', include('users.urls'))
 ]
