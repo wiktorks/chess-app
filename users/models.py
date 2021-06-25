866f94c37c5a0117c107ef12d926627b7cddc15d
WiktorKS <wiktorks1994@gmail.com>
2021-06-25 14:10:40 +0200

    Create User Profile Model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+    
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=CASCADE)
+    matches_won = models.PositiveIntegerField(default=0)
+    matches_lost = models.PositiveIntegerField(default=0)
+
+
