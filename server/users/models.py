8f360a82ff5ecc9169b78ea5755fa92c3165e36a
wiktorks <wiktorks1994@gmail.com>
2021-09-27 21:47:06 +0200

    User profile fix

--- server/users/models.py
+++ server/users/models.py
@@ -8,8 +8,5 @@ User._meta.get_field('email')._unique = True
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=CASCADE)
-    name = models.CharField(max_length=150, unique=True, null=True)
     matches_won = models.PositiveIntegerField(default=0)
     matches_lost = models.PositiveIntegerField(default=0)
-
-
