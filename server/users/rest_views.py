df299865c31cfaaa1d4a027aa71da175a04bab55
wiktorks <wiktorks1994@gmail.com>
2021-10-12 16:15:34 +0200

    Test email view

--- server/users/rest_views.py
+++ server/users/rest_views.py
@@ -8,8 +8,11 @@ from rest_framework.mixins import (
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from users.serializers import UserSerializer, RegisterUserSerializer
+from .tasks import send_email_task
 
 #! queryset i serializer class można sparametryzować metodami self.get_query_set/get_serializer_class
 # Flaga permissions/groups (lepiej groups)
@@ -28,6 +31,21 @@ class UserViewSet(
     serializer_class = UserSerializer
 
 # POST
+
+
 class RegisterUserView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterUserSerializer
+
+
+class ActivateAccountView(APIView):
+    def post(self, request):
+        pass
+
+
+class TestMessageView(APIView):
+    def get(self, request, format=None):
+        send_email_task.delay()
+        return Response({
+            'message': 'Email has been sent to temporal mail!'
+        })
--- server/users/urls.py
+++ server/users/urls.py
@@ -1,19 +1,20 @@
 from django.urls import path
 from django.urls.conf import include
-from users.rest_views import RegisterUserView, UserViewSet
+from users.rest_views import RegisterUserView, UserViewSet, TestMessageView
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-# 
+
 profile_router = DefaultRouter()
 profile_router.register('', UserViewSet, basename='user-profile')
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
+    path('mail/', TestMessageView.as_view(), name='send-mail'),
     path('profiles/', include(profile_router.urls), name='user-profile'),
-    path('register/', RegisterUserView.as_view(), name='user-register')
+    path('register/', RegisterUserView.as_view(), name='user-register'),
+    # path('account/<uidb64>/<token>/', name='email-activate')
 ]
