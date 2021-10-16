76ad36480512eab83dcf7b9b2b574b2d80ab50c4
wiktorks <wiktorks1994@gmail.com>
2021-10-16 18:19:42 +0200

    Create email verification celery task

--- server/users/rest_views.py
+++ server/users/rest_views.py
@@ -10,9 +10,13 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django.contrib.auth.models import User
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.utils.encoding import force_bytes, force_text
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 
+from .tokens import account_activation_token
 from users.serializers import UserSerializer, RegisterUserSerializer
-from .tasks import send_email_task
+from .tasks import send_activation_email_task, send_email_task_test
 
 #! queryset i serializer class można sparametryzować metodami self.get_query_set/get_serializer_class
 # Flaga permissions/groups (lepiej groups)
@@ -30,13 +34,26 @@ class UserViewSet(
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-# POST
-
-
+# uuid4 - standard tworzenia id, generuje losowy ciąg znaków 
 class RegisterUserView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterUserSerializer
 
+    def perform_create(self, serializer):
+        user = self.request.user
+        serializer.save(user=user)
+# wzorzec obserwator
+        current_site = get_current_site(self.request)
+        send_activation_email_task.delay(
+            current_site,
+            urlsafe_base64_encode(force_bytes(user.pk)),
+            account_activation_token.make_token(user),
+            user.email
+        )
+
+# domain.com/subdomain/<id>
+# domain.com/subdomain?id=<id>
+# 19.10: 16:30
 
 class ActivateAccountView(APIView):
     def post(self, request):
@@ -45,7 +62,7 @@ class ActivateAccountView(APIView):
 
 class TestMessageView(APIView):
     def get(self, request, format=None):
-        send_email_task.delay()
+        send_email_task_test.delay()
         return Response({
-            'message': 'Email has been sent to temporal mail!'
+            'message': f'My current site: {get_current_site(request)}'
         })
--- server/users/tasks.py
+++ server/users/tasks.py
@@ -1,12 +1,22 @@
 from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 from celery import shared_task
 
 @shared_task
-def send_email_task():
+def send_email_task_test():
     send_mail(
         'My Django mailer worked!', 
-        'This is a proof that Django mailer works!!!',
+        'This is the proof that my mailer works!!!!',
         'wiktorks1994@gmail.com',
         ['atkt1haj1i@dispostable.com']
     )
-    return None
+
+@shared_task
+def send_activation_email_task(current_site, uidb64, token, receiver_email):
+    activation_url = f'{current_site}/api/auth/activate/{uidb64}/{token}/'
+    send_mail(
+        'Account activation mail',
+        f'Click on this link to activate Your account: {activation_url}',
+        'wiktorks1994@gmail.com',
+        [receiver_email]
+    )
