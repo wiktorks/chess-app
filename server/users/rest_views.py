98a3cf07c6448f7542fd269fe973f8df81c506e0
wiktorks <wiktorks1994@gmail.com>
2021-10-18 13:47:15 +0200

    Add account reset url

--- server/users/rest_views.py
+++ server/users/rest_views.py
@@ -10,13 +10,14 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django.contrib.auth.models import User
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from django.utils.encoding import force_bytes, force_text
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 
 from .tokens import account_activation_token
 from users.serializers import UserSerializer, RegisterUserSerializer
-from .tasks import send_activation_email_task, send_email_task_test
+from .tasks import send_activation_email_task
 
 #! queryset i serializer class można sparametryzować metodami self.get_query_set/get_serializer_class
 # Flaga permissions/groups (lepiej groups)
@@ -40,15 +41,15 @@ class RegisterUserView(CreateAPIView):
     serializer_class = RegisterUserSerializer
 
     def perform_create(self, serializer):
-        user = self.request.user
-        serializer.save(user=user)
+        user = serializer.save(user=self.request.user)
 # wzorzec obserwator
         current_site = get_current_site(self.request)
+        print(f'User email -------> "{user.email}"')
         send_activation_email_task.delay(
-            current_site,
+            current_site.domain,
             urlsafe_base64_encode(force_bytes(user.pk)),
-            account_activation_token.make_token(user),
-            user.email
+            user.email,
+            account_activation_token.make_token(user)
         )
 
 # domain.com/subdomain/<id>
@@ -56,13 +57,19 @@ class RegisterUserView(CreateAPIView):
 # 19.10: 16:30
 
 class ActivateAccountView(APIView):
-    def post(self, request):
-        pass
+    def get(self, request, uidb64, token):
+        user_id = urlsafe_base64_decode(uidb64)
+        user = User.objects.get(pk=user_id)
+
+        if user and account_activation_token.check_token(user, token):
+            user.profile.email_verified = True
+            user.save()
+            return Response({
+                "profile": "Account has been activated"
+            }, status=status.HTTP_200_OK)
+        else:
+            return Response({
+                "error": "User does not exists or invalid token"
+            }, status=status.HTTP_400_BAD_REQUEST)
 
 
-class TestMessageView(APIView):
-    def get(self, request, format=None):
-        send_email_task_test.delay()
-        return Response({
-            'message': f'My current site: {get_current_site(request)}'
-        })
